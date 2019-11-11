25aeab77ee562cb00e5bb72ecdd82e926b6d2bc2
Shyam Sundar <Shyam.Sundar@sumtotalsystems.com>
2019-11-11 17:05:25 -0500

    Wired up apis

--- RESTApiDemo/RESTApiDemo/Controllers/HomeController.cs
+++ RESTApiDemo/RESTApiDemo/Controllers/HomeController.cs
@@ -4,9 +4,12 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RESTApiDemo.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace RESTApiDemo.Controllers
 {
@@ -35,8 +38,14 @@ namespace RESTApiDemo.Controllers
     }
 
     [Authorize(AuthenticationSchemes = "oauth")]
-    public IActionResult Transcript(string userId)
+    public async Task<IActionResult> Transcript(string userId)
     {
+      string accessToken = HttpContext.GetTokenAsync("access_token").Result ?? "No Data";
+      var client = new HttpClient();
+      client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+      string apiUrl = $"https://au11sales.sumtotaldevelopment.net/apis/api/v1/users/{ userId }/transcripts";
+      var response = await client.GetAsync(apiUrl);
+      ViewData["RawResponse"] = await response.Content.ReadAsStringAsync();
       return View("Transcripts", userId);
     }
 
